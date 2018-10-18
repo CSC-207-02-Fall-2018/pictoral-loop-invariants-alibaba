23b2cd2a26651dbadb0889aef519fd4a5e35d5e1
zhaoyuanqi <zhaoyuan@grinnell.edu>
2018-10-18 15:11:22 -0500

    Partition.java

--- Partition.java
+++ Partition.java
@@ -3,60 +3,85 @@ package yuanqi;
 public class Partition {
 
 
-	public static int partition(int[] data) {
+	public static int partition(int a[], int left, int right) {
 
-		int left=0;
-		int right=data.length -1;
 		int l_spot=left;
 		int r_spot=right;
 		int middle =0;
+		
+		while(r_spot > l_spot ) {
 
-		while(r_spot >= l_spot ) {
-
-			while(data[r_spot]>=data[left] && r_spot>=left) {
+			while(r_spot!=l_spot && r_spot>left && a[r_spot]>=a[left]) {
+				r_spot--;
 				
-					r_spot--;
-				System.out.println("r" +data[r_spot]);
-			}
-
-			while(data[l_spot]<=data[left] && l_spot<=data.length-1) {
 			
-					l_spot++;
-					System.out.println("l" + data[l_spot]);
+			}
+
+			while(l_spot!=r_spot && l_spot<=right && a[l_spot]<=a[left]) {
+				l_spot++;
+
 			}
 
 
-			if(r_spot>=l_spot) {
-				int temp = data[r_spot];
-				data[r_spot]= data[l_spot];
-				data[l_spot]=temp;
-			}
+			//if(r_spot>=l_spot) {
+				int temp = a[r_spot];
+				a[r_spot]= a[l_spot];
+				a[l_spot]=temp;
+				
+			//}
 		}
 
+		int temp1 = a[l_spot];
+		a[l_spot]= a[left];
+		a[left]= temp1;
 
-		int temp1 = data[r_spot];
-		data[r_spot]= data[left];
-		data[left]= temp1;
 
+		middle = l_spot;
 
-		middle = r_spot;
-
+		//System.out.println(middle);
 		return middle;
 	}
-
+   /*
 	public static void main(String arg[]) {
 
+		//Test 1: random array of data
 		int data[] = {4,3,1,6,2,5,3,2};
-		int data2[] = {0, -20, 100};
-		partition(data2);
-		//System.out.println("middle: "+partition(data2));
-		for(int i=0; i<3; i++) {
-
-			System.out.println("data: "+ data2[i]); 
+		System.out.println("middle index: "+partition(data,0,7));
+		for(int i=0; i<8; i++) {
+			System.out.println("data: "+ data[i]); 
 		}
+		
+		//Test 2: test avoiding l_left out of bound 
+		int data2[] = {100, 0, -20};
+		System.out.println("middle index: "+partition(data2,0,2));
+		for(int i=0; i<3; i++) {
+			System.out.println("data2: "+ data2[i]); 
+		}
+		
+		//Test 3: Other left and right to start with
+		int data3[] = {4,3,1,6,2,5,3,2};
+		System.out.println("middle index: "+partition(data3,3,6));
+		for(int i=0; i<8; i++) {
+			System.out.println("data3: "+ data3[i]); 
+		}
+		
+		//Test 4: test avoiding r_left out of bound 
+		int data4[] = {200,-20, 0, 100};
+		System.out.println("middle index: "+partition(data4,0,3));
+		for(int i=0; i<4; i++) {
+			System.out.println("data4: "+ data4[i]); 
+		}
+		
+		int data5[] = {3,3,1,2,2};
+		System.out.println("middle index: "+partition(data5,0,4));
+		for(int i=0; i<5; i++) {
+			System.out.println("data4: "+ data5[i]); 
+		}
+		
+		
 
 	}
-
+*/
 
 
 }
