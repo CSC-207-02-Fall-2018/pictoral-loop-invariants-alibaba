adbce4dd3031015c1e987aaf6e913da408b31608
zhaoyuanqi <zhaoyuan@grinnell.edu>
2018-10-19 14:55:50 -0500

    Partition.java

--- Partition.java
+++ Partition.java
@@ -1,88 +1,113 @@
 package yuanqi;
 
+/**
+ * Partition class rearranges the elements of a between a[left] and a[right], 
+ * so that one array element has moved to the place it would belong in a sorted array
+ * by having a method partition(int a[], int left, int right). 
+ * @author Govind
+ * @author Yuanqi 
+ *
+ */
 public class Partition {
 
-    /**
-     * Rearranges the elements of a between a[left] and a[right],
-     *      so that one array element has moved to the place it
-     *      would belong in a sorted array
-     * @param a
-     * @param left
-     * @param right
-     * @return the middle element of the array
-     */
+	/**
+	 * Rearranges the elements of a between a[left] and a[right],
+	 *      so that one array element has moved to the place it
+	 *      would belong in a sorted array.
+	 * @param a		the array needs to be sorted
+	 * @param left	the start index of the subarray which needs to be sorted 
+	 * @param right		the end index of the subarray which needs to be sorted
+	 * @return the index of chosen middle point of the array
+	 */
 	public static int partition(int a[], int left, int right) {
-	    //initializing variables
+		//initializing variables
 		int l_spot=left;
 		int r_spot=right;
 		int middle =0;
 
-		// runs until r_spot and l_spot come together
-		while(r_spot > l_spot) {
-
-			while(r_spot!=l_spot && r_spot>left && a[r_spot]>=a[left]) {
-				r_spot--;
-
-			}
-
-			while(l_spot!=r_spot && l_spot<=right && a[l_spot]<=a[left]) {
-				l_spot++;
-
-			}
-            //swapping l_spot and r_spot
-            int temp = a[r_spot];
-            a[r_spot]= a[l_spot];
-            a[l_spot]=temp;
+		//The case when there is not element in the array.
+		if(a.length == 0) {
+			System.out.println("Error: Empty array!");
+			return 0;
 		}
+		else {
 
-        // placing a[left] in its appropriate position a[middle]
-		int temp1 = a[l_spot];
-		a[l_spot]= a[left];
-		a[left]= temp1;
+			// runs until r_spot surpasses l_spot
+			while(r_spot > l_spot) {
 
-		middle = l_spot;
+				while(r_spot!=l_spot && r_spot>left && a[r_spot]>=a[left]) {
+					r_spot--;
 
-		return middle;
+				}
+
+				while(l_spot!=r_spot && l_spot<=right && a[l_spot]<=a[left]) {
+					l_spot++;
+
+				}
+				//swapping l_spot and r_spot
+				int temp = a[r_spot];
+				a[r_spot]= a[l_spot];
+				a[l_spot]=temp;
+			}
+
+			// placing a[left] in its appropriate position middle
+			int temp1 = a[l_spot];
+			a[l_spot]= a[left];
+			a[left]= temp1;
+
+			middle = l_spot;
+
+			return middle;
+		}
 	}
 
 	public static void main(String arg[]) {
 
 		//Test 1: random array of data
 		int data[] = {4,3,1,6,2,5,3,2};
+		//Expected Output: middle index should be 5.
 		System.out.println("middle index: "+partition(data,0,7));
 		for(int i=0; i<8; i++) {
 			System.out.println("data: "+ data[i]); 
 		}
-		
+
 		//Test 2: test avoiding l_left out of bound 
 		int data2[] = {100, 0, -20};
+		//Expected Output: middle index should be 2.
 		System.out.println("middle index: "+partition(data2,0,2));
 		for(int i=0; i<3; i++) {
 			System.out.println("data2: "+ data2[i]); 
 		}
-		
+
 		//Test 3: Other left and right to start with
 		int data3[] = {4,3,1,6,2,5,3,2};
+		//Expected Output: middle index should be 6.
 		System.out.println("middle index: "+partition(data3,3,6));
 		for(int i=0; i<8; i++) {
 			System.out.println("data3: "+ data3[i]); 
 		}
-		
+
 		//Test 4: test avoiding r_left out of bound 
 		int data4[] = {200,-20, 0, 100};
+		//Expected Output: middle index should be 3.
 		System.out.println("middle index: "+partition(data4,0,3));
 		for(int i=0; i<4; i++) {
 			System.out.println("data4: "+ data4[i]); 
 		}
-		
-        
+
+
 		int data5[] = {3,3,1,2,2};
+		//Expected Output: middle index should be 4.
 		System.out.println("middle index: "+partition(data5,0,4));
 		for(int i=0; i<5; i++) {
 			System.out.println("data5: "+ data5[i]);
 		}
-		
-		
+
+		//Test with empty array
+		int a6[]= {};
+		partition(a6,0,3);
+
+
 
 	}
 
--- Selection.java
+++ Selection.java
@@ -1,66 +1,111 @@
 package yuanqi;
 
+/**
+ * Selection class select the kth smallest element in a specific 
+ * array. It has two main methods: select and median, and a helper
+ * method selecteHelper. Median is a special case of select, when
+ * k=length/2. 
+ * @author Govind
+ * @author Yuanqi 
+ *
+ */
+
 public class Selection extends Partition{
 
-    /**
-     * Keeps track of what subarray, a[left], ..., a[right], should be searched
-     * @param a
-     * @param k
-     * @param left
-     * @param right
-     * @return the k-((middle)-left)-1th smallest element in an array segment a[(middle)+1], ...,a[right]
-     */
+	/**
+	 * Find the kth smallest element in the array and 
+	 * keeps track of what subarray, a[left], ..., a[right], should be searched
+	 * @param a		the array we choose the element from
+	 * @param k		the kth smallest element 
+	 * @param left	the start index of the subarray which needs to be sorted 
+	 * @param right		the end index of the subarray which needs to be sorted
+	 * @return the kth smallest element in an array.
+	 */
 	public static int selectHelper (int a[], int k, int left, int right) {
 
-	    // if only one element
-		if(right - left == 0) {
-			return a[right];
+		//The case when there is not element in the array.
+		if(a.length == 0) {
+			System.out.println("Error: Empty array!");
+			return 0;
 		}
 
-		// rearranges array into a[left] ... a[(middle)] ... a[right]
-		int middle = partition(a,left, right);
+		else {
+			// if only one element in the array
+			if(right - left == 0) {
+				return a[right];
+			}
 
-		// stop when desired element is found
-		if(k == (middle-left+1)) {
-			return a[middle];
-		}
+			// sorts the array
+			int middle = partition(a,left, right);
 
-		// recurse
-		else if(k <= (middle-left)) {
-		
-			return selectHelper(a,k,left,middle-1);
-		}
-		    // recurse
+			// stop when desired element is found
+			if(k == (middle-left+1)) {
+				return a[middle];
+			}
+
+			// recurse
+			else if(k <= (middle-left)) {
+
+				return selectHelper(a,k,left,middle-1);
+			}
+
+			// recurse
 			return selectHelper(a,k - (middle - left) - 1,middle+1,right);
+		}
 	}
 
-    /**
-     * Uses the selectHelper and Partition methods to find the kth smallest element in an array a
-     * @param a
-     * @param k
-     * @return the kth smallest element in an array a
-     */
+
+	/**
+	 * Uses the selectHelper and Partition methods to find the kth smallest element in an array a
+	 * @param a  the array we choose the element from
+	 * @param k   the kth smallest element
+	 * @return the kth smallest element in an array a
+	 */
 	public static int select(int a[], int k) {
-	    // calls selectHelper with the proper starting values
+		// calls selectHelper with the proper starting values
 		return selectHelper (a, k, 0, a.length-1);
 	}
 
-    /**
-     * Finds the median element in an array a
-     * @param a
-     * @return median element
-     */
+
+	/**
+	 * Finds the median element in an array a.
+	 * When the length of the array is even, we round it to the
+	 * lower one to be the median.
+	 * @param a		the array of interest
+	 * @return median element
+	 */
 	public static int median(int a[]) {
+
+
 		return select(a, (a.length/2));
 	}
 
 	public static void main(String arg[]) {
-        int a[] = {4, 3, 1, 6, 2, 5, 3, 2};
-        System.out.println("The element is: " + select(a, 4));
-        System.out.println("The median is: " + median(a));
+		//Test random array with even number of elements
+		int a[] = {4, 3, 1, 6, 2, 5, 3, 2};
+		System.out.println("The element is: " + select(a, 4));
+		System.out.println("The median is: " + median(a));
 
-        int a2[] = {2, 5, 2, 5, 7, 1, 3};
-        System.out.println("The element is: " + select(a2, 1));
-        System.out.println("The median is: " + median(a2));
-    }
+		//Test random array with odd number of elements
+		int a2[] = {2, 5, 2, 5, 7, 1, 3};
+		System.out.println("The element is: " + select(a2, 1));
+		System.out.println("The median is: " + median(a2));
+
+		//Test right side out of bound exception handling
+		int a3[] = {5,4,3,2};
+		System.out.println("The element is: " + select(a3, 2));
+		System.out.println("The median is: " + median(a3));
+
+		//Test left side out of bound exception handling
+		int a4[] = {2,3,4,5};
+		System.out.println("The element is: " + select(a4, 4));
+		System.out.println("The median is: " + median(a4));
+
+
+		//Test selection with empty array
+		int a5[]= {};
+		select(a5,2);
+		median(a5);
+
+	}
 }
